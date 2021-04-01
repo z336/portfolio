1b0d0e7994d80bdc5a6baa70833183d0cc1b8e52
jon <jcoleman1388@gmail.com>
2021-03-31 22:16:27 -0400

    trying sass to remove FOUC

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import 'normalize.css';
+import '../styles/GlobalStyles.scss';
 import styled from 'styled-components';
 import { ThemeProvider } from '../utils/useTheme';
 import SiteContainer from '../styles/Grids';
-import GlobalStyles from '../styles/GlobalStyles';
+// import GlobalStyles from '../styles/GlobalStyles';
 import Header from './Header';
 import Footer from './Footer';
 
@@ -17,7 +18,7 @@ export default function Layout({ children }) {
   return (
     <ThemeProvider>
       <SiteContainer>
-        <GlobalStyles />
+        {/* <GlobalStyles /> */}
         <Header />
         <Main>{children}</Main>
         <Footer />
