628e0e2e3ab8fd092498d141117aba83eea62a3c
jon <jcoleman1388@gmail.com>
2021-04-01 21:15:41 -0400

    home page improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,15 @@ import * as React from 'react';
 import styled from 'styled-components';
 import SEO from '../components/SEO';
 import Article from '../styles/Article';
+import Header from '../styles/Header';
 
 const StyledArticle = styled(Article)`
-  div {
-    padding: 2rem;
+  .cta {
     border: 0.5rem solid;
+  }
+
+  .info {
+    padding: 1.5rem 0 1rem 2rem;
     h2 {
       margin-bottom: 1rem;
     }
@@ -27,29 +31,37 @@ const StyledArticle = styled(Article)`
   }
 `;
 
+const StyledHeader = styled(Header)`
+  padding-left: 2rem;
+`;
+
 export default function IndexPage() {
   return (
     <>
       <SEO title="Jon Coleman" />
       <StyledArticle>
-        <div>
-          <h1>Jon Coleman</h1>
-          <h2>Front-End Designer</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Necessitatibus nemo non corrupti doloremque ratione neque? Illum
-            beatae itaque aliquam nulla molestiae! Nobis ex quos a perferendis
-            sequi quas, sapiente aliquid! I'm testing something out with this
-            extra text pay no mind.
-          </p>
-          <ul>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>
-          </ul>
+        <div className="cta">
+          <StyledHeader>
+            <h1>Jon Coleman</h1>
+          </StyledHeader>
+          <div className="info">
+            <h2>Front-End Designer</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus nemo non corrupti doloremque ratione neque? Illum
+              beatae itaque aliquam nulla molestiae! Nobis ex quos a perferendis
+              sequi quas, sapiente aliquid! I'm testing something out with this
+              extra text pay no mind.
+            </p>
+            <ul>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/projects">Projects</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </StyledArticle>
     </>
