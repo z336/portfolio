a9e79d9c4261301acb3933afa1b5a503a05d3554
jon <jcoleman1388@gmail.com>
2021-04-03 16:51:17 -0400

    ok the header needs more space

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ import ToggleTheme from './ToggleTheme';
 const StyledHeader = styled.header`
   grid-area: header;
   display: flex;
-  padding: 1rem 1rem 0.5rem 1rem;
+  padding: 2rem 1rem 0.5rem 1rem;
   font-size: 1.5rem;
   a {
     text-decoration: none;
