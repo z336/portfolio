e66b7f59bd0cace279ef85512853e057c33750b2
jon <jcoleman1388@gmail.com>
2021-04-04 10:09:03 -0400

    small changes to figure out the reloading bug

--- src/styles/Heading.js
+++ src/styles/Heading.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const Heading = styled.div`
+const Heading = styled.article`
   background: var(--black);
   color: var(--white);
   padding: 1.5rem 1rem 1rem 1rem;
