0b33de48a78bb74413a91ac2939052b6f29fae3b
jon <jcoleman1388@gmail.com>
2021-05-08 17:03:49 -0400

    scrollbar

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -26,7 +26,7 @@ export default function HelmetTemplate({
       {/* Meta Tags */}
       <meta
         name="viewport"
-        content="width=device-width, initial-scale=1.0, shrink-to-fit=no, maximum-scale=1.0"
+        content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
       />
       <meta charSet="utf-8" />
       <meta name="description" content={site.siteMetadata.description} />
